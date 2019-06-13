6b94e0da305c61ff5fe379fbda42dc7dded95317
ach-raf <achraf.zouchou@gmail.com>
2019-06-13 16:24:47 +0100

    not working

--- bibl.py
+++ bibl.py
@@ -13,6 +13,12 @@ class Card:
         else:
             return False
 
+    def is_playable(self, my_board):
+        if self.suit == my_board.current_suit or self.number == my_board.current_number:
+            return True
+        else:
+            return False
+
     def __str__(self):
         """Format: the suit on the left and number on the right"""
         result = '[{0}, {1}]'.format(self.suit, self.number)
@@ -26,8 +32,10 @@ class Player:
     def __init__(self, label):
         self.label = label
         self.hand = []
-        self.num_cards_left = len(self.hand)
         self.last_card_played = Card()
+
+    def num_cards(self):
+        return len(self.hand)
 
     def add_card(self, card):
         self.hand.append(card)
@@ -37,19 +45,16 @@ class Player:
 
     def select_card(self, card_index):
         try:
-            return self.hand[card_index]
+            return self.hand[card_index - 1]
         except IndexError:
             return False
 
     def throw_card(self, card_index):
-        try:
-            # remove the card from the hand of the player and storing it in temp_card
-            temp_card = self.hand.pop(card_index)
-            # last played card by a player
-            self.last_card_played = temp_card
-            return Card(temp_card)
-        except IndexError:
-            return False
+        # remove the card from the hand of the player and storing it in temp_card
+        temp_card = self.hand.pop(card_index - 1)
+        # last played card by a player
+        self.last_card_played = temp_card
+        return temp_card
 
     def __str__(self):
         """Format: the suit on the left and number on the right"""
@@ -120,6 +125,17 @@ class Deck:
         except IndexError:
             return False
 
+    def get_first_card(self):
+        found = False
+        while not found:
+            temp_card = self.cards[len(self.cards) - 1]
+            if temp_card.number not in [1, 2, 7]:
+                found = True
+                self.cards.pop(len(self.cards) - 1)
+                return temp_card
+            else:
+                random.shuffle(self.cards)
+
     def re_shuffle(self, my_board):
         random.shuffle(my_board)
         [self.cards.append(card) for card in my_board]
@@ -134,7 +150,7 @@ class Deck:
 
 def setup_game():
     my_deck = Deck()
-    first_card = my_deck.get_card()
+    first_card = my_deck.get_first_card()
     my_board = Board(first_card)
     number_of_players = int(input('How many players, MAX is 4 '))
     while number_of_players > 4 or number_of_players < 1:
@@ -148,6 +164,13 @@ def setup_game():
             my_players[indx].add_card(my_deck.get_card())
 
     return my_deck, my_board, my_players
+
+
+def compare_cards(card_one, card_two):
+    if card_one.suit == card_two.suit or card_one.number == card_two.number:
+        return True
+    else:
+        return False
 
 
 def show_players(my_players):
@@ -171,6 +194,17 @@ def next_turn():
     current_turn += 1
 
 
+def draw_card():
+    global chosen_card
+    chosen_card = deck.get_card()
+    if not chosen_card:
+        deck.re_shuffle(board)
+        board.clear()
+        chosen_card = deck.get_card()
+    players[current_player].add_card(chosen_card)
+    print('adding {0} to your hand'.format(chosen_card))
+
+
 deck, board, players = setup_game()
 
 print(deck)
@@ -179,29 +213,40 @@ print(board)
 
 current_player = 0
 current_turn = 0
+correct_answer = False
 end_game = False
+SUITS = ['gold', 'sword', 'cup', 'club']
 while not end_game:
-    print('player {} : {}'.format(current_player, players[current_player]))
-    card_choice = int(
-        input('player {0} turn : write the index of the card you wanna play and 0 to draw a card : '.format(
-            current_player)))
-    if card_choice == 0:
-        if deck.get_card():
-            chosen_card = deck.get_card()
+    while not correct_answer:
+        print('player {} : {}'.format(current_player, players[current_player]))
+        card_choice = int(
+            input('player {0} turn : write the index of the card you wanna play and 0 to draw a card : '.format(
+                current_player)))
+        if card_choice == 0:
+            correct_answer = True
+            draw_card()
         else:
-            deck.re_shuffle(board)
-            board.clear()
-            chosen_card = deck.get_card()
-        players[current_player].add_card(chosen_card)
-        print('adding {0} to your hand'.format(chosen_card))
-
-    else:
-        if players[current_player].throw_card(card_choice):
-            chosen_card = players[current_player].throw_card(card_choice)
-            if chosen_card.is_special():
-                print('special')
-            else:
-                board.add_card(chosen_card)
-
-    show_info(board)
-    next_turn()
+            selected_card = players[current_player].select_card(card_choice)
+            if selected_card.is_playable(board):
+                correct_answer = True
+                chosen_card = players[current_player].throw_card(card_choice)
+                print('Played Card: ', chosen_card)
+                if chosen_card.is_special():
+                    if chosen_card.number == 7:
+                        suit_choice = -1
+                        while suit_choice not in range(4):
+                            suit_choice = int(
+                                input(
+                                    'player {0} Choose a suit:\n0 : gold\n1 : swords'
+                                    '\n2 : cups\n3 : clubs\nYour choice : '.format(
+                                        current_player)))
+                        board.add_card(chosen_card)
+                        board.current_suit = SUITS[suit_choice]
+                else:
+                    board.add_card(chosen_card)
+        show_info(board)
+        if players[current_player].num_cards() == 0:
+            print('player {} Wins'.format(current_player))
+            end_game = True
+        else:
+            next_turn()
